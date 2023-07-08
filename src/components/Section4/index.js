3aa7e1a6727105b940732f07093c29fa292cc343
Jonathan Maia <jonathancmaia@gmail.com>
2023-07-07 22:57:00 -0300

    removed duplicated projects cards

--- src/components/Section4/index.js
+++ src/components/Section4/index.js
@@ -1,8 +1,9 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, createElement} from 'react';
 import './style.css';
 
 const Section4 = () => {
     const [response, setResponse] = useState(false);
+    let uniqueUrls = [];
 
     useEffect(() => {
         fetch('https://api.github.com/users/jonathancmaia/repos')
@@ -21,13 +22,23 @@ const Section4 = () => {
                     <div className="projectsHandler">
                         {response.map((p, i) => 
                             {
-                                if (p.homepage) {
+                                //Verify if a project has 2 repositories
+                                if (p.homepage && !uniqueUrls[p.homepage]) {
+
+                                    uniqueUrls[p.homepage] = {
+                                            repositories: [{
+                                            name: p.name,
+                                            fullName: p.full_name
+                                        }]
+                                    };
+
                                     return (
                                         <div className="project" key={i}>
                                             <img src={
                                                 'https://api.microlink.io/?url='+p.homepage+'&screenshot=true&embed=screenshot.url&waitForTiemout=3s'
                                             } alt={p.name} />
-                                            <h2>
+
+                                            <h2 id={p.homepage}>
                                                 <a href={"https://github.com/"+p.full_name} target="_blank" rel="noreferrer" className="rgb" data={p.name+' * '}>
                                                     {p.name} <i className="bi bi-box-arrow-up-right"></i>
                                                 </a>
@@ -45,7 +56,28 @@ const Section4 = () => {
                                                 </a>
                                             </h3>
                                         </div>
-                                    )
+                                    );
+
+                                //if a project has 2 repositories just create a link and push this into projects card
+                                } else if (p.homepage) {
+                                    const a = document.createElement("a");
+                                    a.setAttribute("href", "https://github.com/"+p.full_name);
+                                    a.setAttribute("target", "_blank");
+                                    a.setAttribute("rel", "noreferrer");
+                                    a.setAttribute("class", "rgb");
+                                    a.setAttribute("data", p.name+' * ');
+                                    a.setAttribute("id", p.full_name);
+                                    const aText = document.createTextNode(p.name+" ");
+                                    
+                                    const icon = document.createElement("i");
+                                    icon.setAttribute("class", "bi bi-box-arrow-up-right");
+
+                                    a.appendChild(aText);
+                                    a.appendChild(icon);
+
+                                    if (document.getElementById(p.homepage) !== null && !document.getElementById(p.full_name)) {
+                                        document.getElementById(p.homepage).appendChild(a);
+                                    }
                                 }
                             }
                         )}
--- src/components/Section5/index.js
+++ src/components/Section5/index.js
@@ -13,6 +13,7 @@ const Section5 = () => {
                 Contatos
             </h3>
             <div id="socialHandler">
+                <a href="https://www.linkedin.com/in/jonathan-antonio-cavalcanti-maia-658716145/" target="_blank" rel="noreferrer">
                 <motion.div
                     whileHover={{ scale: 1.2 }}
                     whileTap={{ scale: 0.9 }}
@@ -24,70 +25,79 @@ const Section5 = () => {
                             Linkedin
                         </h4>
                         <p>
-                            <a href="https://www.linkedin.com/in/jonathan-antonio-cavalcanti-maia-658716145/" target="_blank" rel="noreferrer">in/jonathan-antonio-cavalcanti-maia-658716145</a>
+                            in/jonathan-antonio-cavalcanti-maia-658716145
                         </p>
                     </div>
                 </motion.div>
-                <motion.div
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.9 }}
-                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                >
-                    <i className="bi bi-github rgb" data="***"></i>
-                    <div>
-                        <h4>
-                            GitHub
-                        </h4>
-                        <p>
-                            <a href="https://github.com/Jonathancmaia" target="_blank" rel="noreferrer">github.com/Jonathancmaia</a>
-                        </p>
-                    </div>
-                </motion.div>
-                <motion.div
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.9 }}
-                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                >
-                    <i className="bi bi-whatsapp rgb" data="***"></i>
-                    <div>
-                        <h4>
-                            WhatsApp
-                        </h4>
-                        <p>
-                            <a href="https://wa.me/+55021980892433" target="_blank" rel="noreferrer">wa.me/+55021980892433</a>
-                        </p>
-                    </div>
-                </motion.div>
-                <motion.div
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.9 }}
-                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                >
-                    <i className="bi bi-envelope-at rgb" data="***"></i>
-                    <div>
-                        <h4>
-                            E-mail
-                        </h4>
-                        <p>
-                            <a href="mailto:jonathancmaia@gmail.com" target="_blank" rel="noreferrer">jonathancmaia@gmail.com</a>
-                        </p>
-                    </div>
-                </motion.div>
-                <motion.div
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.9 }}
-                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                >
-                    <i className="bi bi-phone rgb" data="***"></i>
-                    <div>
-                        <h4>
-                            Telefone
-                        </h4>
-                        <p>
-                            <a href="tel:+5521980892433" target="_blank" rel="noreferrer">+55 (021) 9 8089-2433</a>
-                        </p>
-                    </div>
-                </motion.div>
+                </a>
+                <a href="https://github.com/Jonathancmaia" target="_blank" rel="noreferrer">
+                    <motion.div
+                        whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                    >
+                        <i className="bi bi-github rgb" data="***"></i>
+                        <div>
+                            <h4>
+                                GitHub
+                            </h4>
+                            <p>
+                                github.com/Jonathancmaia
+                            </p>
+                        </div>
+                    </motion.div>
+                </a>
+                <a href="https://wa.me/+55021980892433" target="_blank" rel="noreferrer">
+                    <motion.div
+                        whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                    >
+                        <i className="bi bi-whatsapp rgb" data="***"></i>
+                        <div>
+                            <h4>
+                                WhatsApp
+                            </h4>
+                            <p>
+                                wa.me/+55021980892433
+                            </p>
+                        </div>
+                    </motion.div>
+                </a>
+                <a href="mailto:jonathancmaia@gmail.com" target="_blank" rel="noreferrer">
+                    <motion.div
+                        whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                    >
+                        <i className="bi bi-envelope-at rgb" data="***"></i>
+                        <div>
+                            <h4>
+                                E-mail
+                            </h4>
+                            <p>
+                                jonathancmaia@gmail.com
+                            </p>
+                        </div>
+                    </motion.div>
+                </a>
+                <a href="tel:+5521980892433" target="_blank" rel="noreferrer">
+                    <motion.div
+                        whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                    >
+                        <i className="bi bi-phone rgb" data="***"></i>
+                        <div>
+                            <h4>
+                                Telefone
+                            </h4>
+                            <p>
+                                +55 (021) 9 8089-2433
+                            </p>
+                        </div>
+                    </motion.div>
+                </a>
             </div>
         </motion.section>
     );
